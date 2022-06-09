2625917160a5c2dd8083b08f4672d70aeeca0983
ShazaALi <shazamamdouh@aucegypt.edu>
2022-06-09 17:52:35 +0200

    “message”

--- main.cpp
+++ main.cpp
@@ -1,26 +1,11 @@
 #include<iostream>
 using namespace std;
+#include"Header.h"
 
-#include "function1.h"
-int main(){
-    int n;
-    cin>>n;
-    int** arr=new int*[n];
-    for(int i=0; i<n; i++){
-        arr[i]=new int[n];
-        for(int j=0; j<n; j++){
-            arr[i][j]=0;
-        }
-    }
-    if(nQueen(arr, 0, n)){
-        for(int i=0; i<n; i++){
 
-        for(int j=0; j<n; j++){
-            cout<<arr[i][j]<<" "; 
-        }cout<<endl;
-        }
-    }
+int main()
 
-    
+{
+    solveNQ();
     return 0;
 }
